764eb9db9787265013ce66a6754b5d91b817d6e0
Mayank Pathak <44158581+onlinejudge95@users.noreply.github.com>
2024-09-29 15:33:57 +0530

    Adds hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { Hero } from '@/components/LandingPage/hero';
 import { NavigationMenu } from '@/components/navigationMenu';
 
 const Home = () => {
-  return <NavigationMenu />;
+  return (
+    <>
+      <NavigationMenu />
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
