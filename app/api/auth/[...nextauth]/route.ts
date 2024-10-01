1ed9eef68e8b7de6e621b791d2176aff44e8a8a4
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-10-02 01:04:47 +0530

    Adds user management (#11)
    * Adds prisma along with users table with upsert at login
    
    * Adds dashboard component skeleton

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,9 @@ import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import TwitchProvider from 'next-auth/providers/twitch';
 
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
+
 const handler = NextAuth({
   providers: [
     DiscordProvider({
@@ -38,9 +41,38 @@ const handler = NextAuth({
         throw new Error('Failed to fecth profile info from google');
       }
 
-      // Do custom user upsert logic
-      console.log('AUTHENTICATED');
+      await db.user.upsert({
+        where: { email: user.email },
+        create: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          avatar: user.image,
+        },
+        update: {
+          name: user.name,
+          avatar: user.image,
+        },
+      });
       return true;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+    async jwt({ profile, token }) {
+      if (profile) {
+        const user = await db.user.findUnique({
+          where: {
+            email: profile.email,
+          },
+        });
+        if (!user) {
+          throw new Error('No user exists');
+        }
+        token.id = user.id;
+      }
+      return token;
     },
   },
   debug: process.env.NODE_ENV === 'development',
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <section className='border-b min-h-screen'>
+      <p>Hello, World!!</p>
+    </section>
+  );
+};
+
+export default Dashboard;
