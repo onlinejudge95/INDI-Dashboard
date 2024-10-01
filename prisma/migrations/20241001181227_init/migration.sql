a6c72fc2ea11e2303f8286479f6b7dbe6d3393a0
Mayank Pathak <44158581+onlinejudge95@users.noreply.github.com>
2024-10-02 00:41:10 +0530

    Adds prisma along with users table with upsert at login

---
+++ prisma/migrations/20241001181227_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "avatar" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
