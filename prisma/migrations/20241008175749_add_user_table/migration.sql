ecc846ee8a30ac40a61d3f9501fa58c025a0f710
Mayank Pathak <44158581+onlinejudge95@users.noreply.github.com>
2024-10-08 23:29:20 +0530

    Uses postgres for local development

--- prisma/migrations/20241008175749_add_user_table/migration.sql
+++ prisma/migrations/20241008175749_add_user_table/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
     "avatar" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
